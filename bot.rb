e1777044e7c1b25491f04683147d4b8030ccefd4
Marco Roth <marco.roth@intergga.ch>
2020-10-24 19:05:33 +0200

    optimize regex to scan for all defined repos instead of looping over every repo

--- bot.rb
+++ bot.rb
@@ -17,30 +17,31 @@ MAPPINGS = {
   'Expo': 'hopsoft/stimulus_reflex_expo'
 }.freeze
 
-MAPPINGS.each do |prefix, repo|
-  bot.message(contains: "#{prefix}#") do |event|
-    event.message.content.scan(/#{prefix}#\d+/).each do |reference|
-      _, number = reference.split('#')
+prefixes = MAPPINGS.keys.join('|')
+regex = /((#{prefixes})#(\d+))/
 
-      begin
-        gh_repo = client.repo repo
-        data = gh_repo.rels[:issues].get(uri: { number: number }).data
+bot.message(contains: regex) do |event|
+  event.message.content.scan(regex).each do |reference, prefix, number|
+    repo_name = MAPPINGS[prefix.to_sym]
 
-        title = data[:title]
-        user = data[:user][:login]
-        url = data[:html_url]
+    begin
+      repo = client.repo(repo_name)
+      data = repo.rels[:issues].get(uri: { number: number }).data
 
-        type = url.include?('pull') ? 'PR' : 'Issue'
+      title = data[:title]
+      user = data[:user][:login]
+      url = data[:html_url]
 
-        response = "`#{reference}` - #{type} by @#{user}: \"#{title}\" \n[#{url}]"
-      rescue Octokit::NotFound
-        response = "`#{reference}` isn't an issue or pull request in `#{repo}`"
-      rescue StandardError
-        response = "https://github.com/#{repo}/issues/#{number}"
-      end
+      type = url.include?('pull') ? 'PR' : 'Issue'
 
-      event.respond(response)
+      response = "`#{reference}` - #{type} by @#{user}: \"#{title}\" \n[#{url}]"
+    rescue Octokit::NotFound
+      response = "`#{reference}` isn't an issue or pull request in `#{repo_name}`"
+    rescue StandardError
+      response = "https://github.com/#{repo_name}/issues/#{number}"
     end
+
+    event.respond(response)
   end
 end
 
